47c8f4f1e3b7cbd3bf1e711ed8278d619e60f8df
Almaz <almazzzik74@yandex.ru>
2021-08-04 19:22:23 +0300

    change calculate method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,8 +39,34 @@ export class AppComponent implements OnInit {
      }
     ];
 
+    // const maxInvoice = this.barData.sort((a,b) => b.invoice - a.invoice)[0].invoice;
+    const maxInvoice: number = this.getMaxInvoice(this.barData);
+    console.log(maxInvoice);
     this.barData.forEach(item => {
-      item.rate = item.invoice*100/3000000;
+      item.rate = item.invoice*100/maxInvoice;
     })
+  }
+
+  // ---- sort Array with SHELL SORT and return max invoice
+  private getMaxInvoice(arr: MonthInvoice[]): number {
+    const n = arr.length;
+    for (let gap = Math.floor(n/2); gap > 0; gap = Math.floor(gap/2)) {
+      for (let i = gap; i < n; i++) {
+        const element = arr[i];
+        let j;
+        for (j=i; j>=gap && arr[j-gap]>element; j-=gap) {
+          arr[j]=arr[j-gap];
+        }
+        arr[j] = element;
+      }
+    }
+
+    const digitsCount = String(arr[0].invoice.toFixed()).trim().length;
+    const realDegreeOfNumber = Math.pow(10, digitsCount);
+
+    const numberToCeil = arr[0].invoice/(realDegreeOfNumber/10);
+    const maxInvoice = Math.ceil(numberToCeil)*realDegreeOfNumber/10;
+    console.log(numberToCeil);
+    return maxInvoice;
   }
 }
